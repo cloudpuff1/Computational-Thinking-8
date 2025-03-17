00eb332d91405b2dd2696c95cb481322a1530a0e
cloudpuff1 <garimabeers@seattleacademy.org>
2025-03-17 20:26:50 +0000

    project1 submission

---
+++ Projects/Priject1.py
@@ -0,0 +1,25 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+stage.set_background("winter")
+
+q1=codesters.Square(100,100,200,'thistle')
+q2=codesters.Square(-100,100,200,'gold')
+q3=codesters.Square(-100,-100,200,'crimson')
+q4=codesters.Square(100,-100,200,'turquoise')
+
+s1=codesters.Sprite("goldenspiral",100,100)
+s1.set_size(0.35)
+s2=codesters.Sprite("cardinal",-100,-100)
+s2.set_size(0.5)
+s3=codesters.Sprite("cardinal",100,-100)
+s3.set_size(0.5)
+s4=codesters.Sprite("cardinal",-100,100)
+s4.set_size(0.5)
+
+message1=codesters.Text("Garima",0,220,"red")
+message2=codesters.Text("this is my message",0,-220,"black")
