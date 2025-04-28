d2823f8e55ba433493542cc3f5cb280d8ce88bf5
cloudpuff1 <garimabeers@seattleacademy.org>
2025-04-28 17:23:04 +0000

    Submission_2_Art.py

--- Art.py
+++ Art.py
@@ -1,19 +1,22 @@
 
 # ###############################################
-# ### SETUP ###
+# ### SETUP ### #
 import turtle
 # ###############################################
 t = turtle.Turtle()
 t.penup()
+# ### STARTING_POSITION ### #
 t.goto(-100,-100)
+# ### SET_SPEED_TO_MAX ### #
 t.speed(10)
+# ### SETUP_COLORS ### #
 t.color("purple")
 turtle.Screen().bgcolor("black")
 t.pendown()
 
 
 colors=["purple","cyan","thistle","turquoise"]
-
+# ### DRAW_SHAPE ### #
 for i in range (40):
     t.color(colors[i%4])
     t.forward(100)
